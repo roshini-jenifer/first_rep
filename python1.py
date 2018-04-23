5fdaa0d2fd83de9a32f0df8b15ebefb3b4c8f82f
roshini-jenifer <roshini.jeniferraj3@gmail.com>
2018-04-23 15:56:23 +0530

    my fifth commmit

--- python1.py
+++ python1.py
@@ -1,6 +1,22 @@
-# This program prints Hello, world!
+import boto3;
 
+dynamodb = boto3.client('dynamodb');
 
-def funk( event, content):
+response = dynamodb.create_table(
+AttributeDefinitions=[
+{	'AttributeName' : 'string',
+	'AttributeType' : 'S'},
+],
+TableName='Halloo',
+KeySchema=[
+{	'AttributeName' : 'string',
+	'KeyType' : 'HASH'},
+},
+],
+ProvisionedThroughput={
+	'ReadCapacityUnits':1,
+	'WriteCapacityUnits':2
+}
+);
+	
 
-	print('Hello, world it is a bad world!');
